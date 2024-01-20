46357d8ab11badfc4c84a73288e0cd9f6df28e1d
Anita Chengalva <anita.chengalva@gmail.com>
2024-01-20 05:18:45 -0800

    listed imports for navbar

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from "react"
+import { HiOutlineBars3 } from "react-icons/hi2"
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 
 const Navbar = () => {
-  return (
+  
+    return (
     <div>Navbar</div>
   )
 }
