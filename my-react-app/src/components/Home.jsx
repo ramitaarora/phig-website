56a23dba2430e8c5835d11ee4ed0ac17aea89fb2
Anita Chengalva <anita.chengalva@gmail.com>
2024-01-20 04:39:02 -0800

    populated home page html, inserted bg image

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import Navbar from './Navbar'
+import BannerBackground from "../assets/PHIG_spring_2023_butterfly_habitat_1.png"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+        <Navbar />
+        <div className='home-banner-container'>
+            <div className='home-bannerbg-container'>
+                <img src={BannerBackground} alt='PHIG Spring Butterfly Habitat' />
+            </div>
+            <div className='home-text-section'>
+                <h1 className='primary-heading'>Living Landscapes</h1>
+                <p className='primary-text'>A California Habitats Garden that protects and conserves vegetation & wildlife critical to our planet.</p>
+                <button className='secondary-button'>
+                    Our Impact
+                </button>
+            </div>
+        </div>
+    </div>
   )
 }
 
