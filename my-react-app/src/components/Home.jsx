c807808c3bc59b637a5104529245476696e80d77
Anita Chengalva <anita.chengalva@gmail.com>
2024-01-20 07:46:59 -0800

    troubleshooting bg resizing

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -7,15 +7,15 @@ const Home = () => {
     <div className='home-container'>
         <Navbar />
         <div className='home-banner-container'>
-            <div className='home-bannerbg-container'>
-                <img src={BannerBackground} alt='PHIG Spring Butterfly Habitat' />
-            </div>
+            <div>
+                <img className='bgImg' src={BannerBackground} alt='PHIG Spring Butterfly Habitat' />
             <div className='home-text-section'>
                 <h1 className='primary-heading'>Living Landscapes</h1>
                 <p className='primary-text'>A California Habitats Garden that protects and conserves vegetation & wildlife critical to our planet.</p>
                 <button className='secondary-button'>
                     Our Impact
                 </button>
+                </div>
             </div>
         </div>
     </div>
