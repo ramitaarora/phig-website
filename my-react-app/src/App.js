14199931e27b1c2ed3386450b79aff2e8959679c
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-25 15:59:29 -0800

    footer

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Home from "./components/Home"
-import VolunteerPage from './components/VolunteerPage'
+import Home from "./components/Home";
+import VolunteerPage from './components/VolunteerPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <VolunteerPage />
+      <Footer />
     </div>
   );
 }
