089e5c264c44d95d00d1ee07826ef8af641d0f1d
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-29 14:25:55 -0800

    finished volunteer page and footer

--- my-react-app/src/components/SearchResult.jsx
+++ my-react-app/src/components/SearchResult.jsx
@@ -1,13 +1,17 @@
 import { css } from '@emotion/css'
 
-export default function SearchResult({ result }) {
+export default function SearchResult({ result, index }) {
     return (
-        <div id="each-result" className={css`display: flex; width: 600px; margin: 10px; border: 1px solid darkgrey; background: lightgrey;`}>
-            <img src="./" alt="result-image" />
+        <div id={"each-result-" + index} className={css`display: flex; height: 250px; width: 700px; margin: 10px; background: lightgrey;`}>
 
-            <div id="result-right" className={css`padding: 20px;`}>
+            <div id="result-img-container" className={css`height: 100%; width: 35%; object-fit: cover; object-position: center; overflow: hidden;`}>
+                <img src={result.image} alt="result-image" />
+            </div>
 
-                <div id="result-corner" className={css`float: right; margin-left: 20px; height: 70px; width: 130px; display: flex; flex-direction: column; justify-content: space-evenly;`}>
+
+            <div id="result-right" className={css`padding: 20px; width: 65%; height: 100%;`}>
+
+                <div id="result-corner" className={css`float: right; margin: 0 20px; height: 70px; width: 25%; display: flex; flex-direction: column; justify-content: space-evenly;`}>
                     <div id="each-corner-stat" className={css`display: flex; justify-content: flex-start; align-items: center;`}>
                         <img src={result.status === "COMPLETED" ? './images/red-circle.svg' : './images/green-circle.svg'} alt="status" />
                         <p className={css`margin-left: 5px;`}>{result.status}</p>
@@ -22,15 +26,16 @@ export default function SearchResult({ result }) {
                     </div>
                 </div>
 
-                <div id="result-details" className={css`display: flex; flex-direction: column; height: 120%;`}>
-                    <h4 className={css`margin-top: 10px;`}>{result.title}</h4>
-                    <h5 className={css`margin-top: 10px;`}>{result.time}</h5>
-                    <p className={css`margin-top: 10px;`}>{result.description}</p>
-
-                <div id="result-buttons" className={css`margin: 20px auto; width: 100%;  display: flex; justify-content: space-evenly; align-items: center;`}>
-                    <button className={css`padding: 5px; color: white; background-color: darkgreen; border: 1px solid darkgreen; border-radius: 5px; cursor: pointer;`}>Apply Now</button>
-                    <p>See Details</p>
-                </div>
+                <div id="result-middle" className={css`height: 100%; display: flex; flex-direction: column; justify-content: center;`}>
+                    <div id="result-details" className={css`display: flex; flex-direction: column; height: fit-content;`}>
+                        <h4 className={css`margin-top: 10px;`}>{result.title}</h4>
+                        <h5 className={css`margin-top: 10px;`}>{result.time}</h5>
+                        <p className={css`margin-top: 10px; font-size: 80%;`}>{result.description}</p>
+                    </div>
+                    <div id="result-buttons" className={css`margin: 20px auto; width: 75%;  display: flex; justify-content: space-evenly; align-items: center;`}>
+                        <button className={css`padding: 5px; color: white; background-color: darkgreen; border: 1px solid darkgreen; border-radius: 5px; cursor: pointer;`}>Apply Now</button>
+                        <p>See Details</p>
+                    </div>
 
                 </div>
 
--- my-react-app/src/components/VolunteerPage.jsx
+++ my-react-app/src/components/VolunteerPage.jsx
@@ -10,7 +10,7 @@ const exampleResults = [
         status: "ONGOING",
         place: "In-Person",
         location: "California",
-        image: './'
+        image: './images/volunteer-sample-1.png'
     },
     {
         title: "UX/UI Designer",
@@ -19,7 +19,7 @@ const exampleResults = [
         status: "COMPLETED",
         place: "Remote",
         location: "California",
-        image: './'
+        image: './images/volunteer-sample-2.png'
     }
 ]
 
@@ -28,103 +28,83 @@ export default function VolunteerPage() {
         <div>
             <Navbar />
 
-            {/*<header>
-                <div className='home-banner-container'>
-                    <div className='hero-image'>
-                        <div className='home-text-section'>
-                            <h1 className='primary-heading'>Living Landscapes</h1>
-                            <p className='primary-text'>A California Habitats Garden that protects and conserves vegetation & wildlife critical to our planet.</p>
-                            <button className='secondary-button'>
-                                Our Impact
-                            </button>
-                        </div>
+            <header>
+                <div id="header-image" className={css`background-image: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url('./images/forest-banner.png'); height: 400px; width: 100vw; background-position: center; background-repeat: no-repeat; background-size: cover;`}>
+                    <div id="header-text" className={css`text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: space-evenly; color: white; height: 300px; width: 100vw;`}>
+                        <h1>Join Us!</h1>
+                        <h2>🌱 Animals need nature, and we need each other 🌿</h2>
+                        <p>To protect the future of the world’s ecosystems, we need everyone on board — from governments, to business owners, environmental activists, corporations, and citizens around the world!</p>
                     </div>
                 </div>
-            </header> */}
+            </header>
 
             <main>
-                <section id="why-volunteer" className={css`display: flex; justify-content: space-evenly; height: 400px; margin: 100px 100px;
-                `}>
-                    <div id="volunteer-left" className={css`display: flex; flex-direction: column; justify-content: space-evenly; padding: 0 50px;
-                    `}>
+                <section id="why-volunteer" className={css`display: flex; justify-content: space-evenly; height: 400px; margin: 100px 100px;`}>
+                    <div id="volunteer-left" className={css`display: flex; flex-direction: column; justify-content: space-evenly; padding: 0 50px;`}>
                         <h2>Why volunteer with us?</h2>
                         <p><b>Water Conservation:</b> Contribute to water conservation efforts by learning and implementing eco-friendly watering techniques. Help spread awareness about responsible water usage in gardening.</p>
                         <p><b>Wildlife Protection:</b> Create safe havens for local wildlife by incorporating wildlife-friendly elements into your garden. From pollinator-friendly plants to bird-friendly feeders, be a vital part of nurturing biodiversity.</p>
                         <p><b>Gardening Tips:</b> Share your gardening expertise and tips with a global audience. Help beginners kickstart their green journey and guide seasoned gardeners toward more sustainable practices.</p>
                     </div>
-                    <div id="volunteer-right">
+                    <div id="volunteer-right" className={css`height: 100%; object-fit: cover; object-position: center; overflow: hidden;`}>
                         <img src="./images/volunteers.png" alt="volunteers" />
                     </div>
                 </section>
 
-                <section id="get-involved" className={css`background-color: darkgreen; color: white; padding: 50px; display: flex; flex-direction: column; justify-content: space-between; height: 250px;
-                `}>
+                <section id="get-involved" className={css`background-image: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url('./images/forest-banner.png'); color: white; padding: 50px; display: flex; flex-direction: column; justify-content: space-between; height: 250px; background-position: center; background-repeat: no-repeat; background-size: cover;`}>
                     <h2>How to Get Involved:</h2>
                     <p>Ready to dig in? Select your favorite cause and let us know how you'd like to contribute. Whether you're a seasoned gardener, a nature enthusiast, or someone eager to make a positive impact, there's a place for you in our community.</p>
                     <p>Let's grow a sustainable future together! 🌍🌻</p>
                 </section>
 
-                <section id="search-opportunities" className={css`
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    margin: 50px auto;
-                `}>
+                <section id="search-opportunities" className={css`display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 50px auto;`}>
                     <h2>Search for Opportunities</h2>
-                    <form className={css`
-                        display: flex;
-                        justify-content: space-evenly;
-                        margin: 30px auto;
-                    `}>
+                    <form className={css`display: flex; justify-content: space-evenly; margin: 30px auto;`}>
                         <input type="text" />
                         <input type="submit" />
                     </form>
 
-                    <div id="results" className={css`
-                        display: flex;
-                        justify-content: space-evenly;
-                        margin: 30px auto;
-                    `}>
-                        <form>
+                    <div id="results" className={css`display: flex; justify-content: space-evenly; margin: 30px auto;`}>
+                        <form className={css`margin: 0 50px 0 0;`}>
                             <div id="work-format">
-                                <h3>Work Format</h3>
-                                <div>
-                                    <input type="checkbox" name="remote-work" />
+                                <h3 className={css`border-bottom: 1px solid lightgrey; margin: 20px 0; padding: 10px 0;`}>Work Format</h3>
+                                <div className={css`margin: 10px 0;`}>
+                                    <input type="checkbox" name="remote-work" className={css`margin: 0 10px;`}/>
                                     <label htmlFor='remote-work'>Remote Work</label>
                                 </div>
-                                <div>
-                                    <input type="checkbox" name="inperson-work" />
+                                <div className={css`margin: 10px 0;`}>
+                                    <input type="checkbox" name="inperson-work" className={css`margin: 0 10px;`}/>
                                     <label htmlFor='inperson-work'>In-Person Work</label>
                                 </div>
                             </div>
 
                             <div id="activity-type">
-                                <h3>Volunteer Activity Type</h3>
-                                <div>
-                                    <input type="checkbox" name="ui-ux" />
+                                <h3 className={css`border-bottom: 1px solid lightgrey; margin: 20px 0; padding: 10px 0;`}>Volunteer Activity Type</h3>
+                                <div className={css`margin: 10px 0;`}>
+                                    <input type="checkbox" name="ui-ux" className={css`margin: 0 10px;`}/>
                                     <label htmlFor='ui-ux'>UI/UX Designer</label>
                                 </div>
-                                <div>
-                                    <input type="checkbox" name="composter" />
+                                <div className={css`margin: 10px 0;`}>
+                                    <input type="checkbox" name="composter" className={css`margin: 0 10px;`}/>
                                     <label htmlFor='composter'>Composter</label>
                                 </div>
-                                <div>
-                                    <input type="checkbox" name="work-participant" />
+                                <div className={css`margin: 10px 0;`}>
+                                    <input type="checkbox" name="work-participant" className={css`margin: 0 10px;`}/>
                                     <label htmlFor='work-participant'>Work Party Participant</label>
                                 </div>
                             </div>
 
-                            <input type="submit" />
-                            <input type="reset" />
+                            <input type="reset" value="Clear Filters" className={css`margin-top: 20px; background-color: #081821; color: white; padding: 8px; border: 1px solid #081821; border-radius: 10px; cursor: pointer;`}/>
                         </form>
 
                         <div id="search-results">
                             <h3>Showing # Results</h3>
                             <div id="results-container">
 
-                                {exampleResults.map(result =>
-                                    <SearchResult result={result} />
+                                {exampleResults.map((result, index) =>
+                                    <div key={index}>
+                                        <SearchResult result={result} index={index}/>
+                                    </div>
                                 )}
 
                             </div>
