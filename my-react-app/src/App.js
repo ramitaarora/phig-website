cc2b3f64db474e9060e381d02e639d8f5328d618
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-30 09:50:47 -0800

    react router working

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Home from "./components/Home";
-import VolunteerPage from './components/VolunteerPage';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <VolunteerPage />
+      <Navbar />
+      <Outlet />
       <Footer />
     </div>
   );
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import App from './App';
+import Home from './pages/Home';
+import VolunteerPage from './pages/VolunteerPage';
+import Error from './pages/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/volunteer" element={<VolunteerPage />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
