ea9528472873b57be98a5a7c15f89c9dcfe044fe
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-02-09 09:31:18 -0800

    suggestions component

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,3 +1,21 @@
 export default function Suggestions() {
-    return;
+    return (
+        <div id="suggestions">
+            <div>
+                <h2>Have a suggestion?</h2>
+                <p>Let us know your thoughts!</p>
+
+                <form id="suggestions-form">
+                    <input type="email" placeholder="Email" />
+                    <input type="textarea" placeholder="Your thoughts" />
+                    <input type="submit" />
+                </form>
+            </div>
+
+            <div>
+                <img src="./images/suggestions.png" alt="suggestions" />
+            </div>
+
+        </div>
+    )
 }
--- src/pages/ResourcesPage.jsx
+++ src/pages/ResourcesPage.jsx
@@ -1,40 +1,41 @@
 import { Link } from 'react-router-dom';
 import Suggestions from '../components/Suggestions';
+import { css } from '@emotion/css';
 
 export default function ResourcesPage() {
     return (
         <div>
-            <header>
+            <header className={css`margin-top: 100px; text-align: center; height: 200px; display: flex; flex-direction: column; align-items: center; justify-content: space-evenly;`}>
                 <h1>Resources</h1>
                 <p>In-depth guides and resources to help you along your journey</p>
             </header>
 
-            <div id="resources">
+            <div id="resources" className={css`display:flex; flex-wrap: wrap; justify-content: space-evenly;`}>
 
-                <Link>
+                <Link to='/resources/websites'>
                     <div id="resource-button">
-                        <img src="" alt="websites" />
+                        <img src="./images/resources/website.png" alt="websites" />
                         <p>Websites</p>
                     </div>
                 </Link>
 
                 <Link>
                     <div id="resource-button">
-                        <img src="" alt="books-cds" />
+                        <img src="./images/resources/book.png" alt="books-cds" />
                         <p>Books and CD's</p>
                     </div>
                 </Link>
 
                 <Link>
                     <div id="resource-button">
-                        <img src="" alt="classes" />
+                        <img src="./images/resources/class.png" alt="classes" />
                         <p>Classes</p>
                     </div>
                 </Link>
 
                 <Link>
                     <div id="resource-button">
-                        <img src="" alt="nurseries" />
+                        <img src="./images/resources/plant.png" alt="nurseries" />
                         <p>Nurseries</p>
                     </div>
                 </Link>
