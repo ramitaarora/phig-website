26602bd4990277ee3f0ac292f6daca8f44337bab
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-02-28 10:51:24 -0800

    confirmation component

---
+++ src/components/Confirmation.jsx
@@ -0,0 +1,9 @@
+export default function Confirmation() {
+    return (
+        <div id="confirmation">
+            <h2>🎉 All set! 🎉</h2>
+            <p>We’ll contact you via email or phone soon</p>
+            <button>Close</button>
+        </div>
+    )
+}
--- src/components/VolunteerApplication.jsx
+++ src/components/VolunteerApplication.jsx
@@ -2,15 +2,19 @@ import { css } from '@emotion/css';
 import { Link } from 'react-router-dom';
 
 export default function VolunteerApplication() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <div id="volunteer-app" className={css`margin: 50px auto; width: 500px;`}>
             <header className={css`text-align: center;`}>
                 <h1>Volunteer Application</h1>
                 <p>We sincerely appreciate you taking the time to fill out this form and for volunteering. It means a lot to us!</p>
             </header>
-            <form className={css` margin: 20px auto; display: flex; flex-direction: column;`}>
+            <form onSubmit={handleSubmit} className={css` margin: 20px auto; display: flex; flex-direction: column;`}>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" placeholder="Name" />
+                <input type="text" id="name" placeholder="Name" required/>
 
                 <p>How should we contact you?</p>
                 <div className={css`display: flex; justify-content: space-around; align-items: center;`}>
@@ -25,10 +29,10 @@ export default function VolunteerApplication() {
                 </div>
 
                 <label htmlFor="email">Email Address</label>
-                <input type="text" id="email" placeholder="Email Address" />
+                <input type="text" id="email" placeholder="Email Address" required/>
 
                 <label htmlFor="phone">Phone Number</label>
-                <input type="tel" id="phone" placeholder="Phone Number" />
+                <input type="tel" id="phone" placeholder="Phone Number" required/>
 
                 <div className={css`display: flex; justify-content: space-between; align-items: center;`}>
                     <div className={css`display: flex; flex-direction: column;`}>
@@ -37,12 +41,12 @@ export default function VolunteerApplication() {
                     </div>
                     <div className={css`display: flex; flex-direction: column;`}>
                         <label htmlFor="availability">When are you open?</label>
-                        <input type="time" id="availability" />
+                        <input type="time" id="availability"  required/>
                     </div>
                 </div>
 
                 <label htmlFor="project">I am available for these projects:</label>
-                <select id="project">
+                <select id="project" required>
                     <option></option>
                 </select>
 
