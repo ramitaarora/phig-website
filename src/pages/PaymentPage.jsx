a28dc1ac840d4463133203364ea13f8cd3c77ce8
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-03-08 20:35:43 -0800

    payment page iframe

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,3 +1,26 @@
 export default function PaymentPage() {
-    return;
+    const divStyle = {
+        position: "relative", 
+        overflow: "hidden", 
+        width: "100%", 
+        height: "1200px", 
+        paddingTop: "100%"
+    }
+
+    const frameStyle = {
+        position: "absolute",
+        border: 0,
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        width: "100%",
+        height: "100%"
+    }
+
+    return (
+        <div style={divStyle}>
+            <iframe title='Donation form powered by Zeffy' style={frameStyle} src='https://www.zeffy.com/en-US/embed/donation-form/cfa8c571-734f-443a-a3b8-5b0a30ba0618' allowpaymentrequest allowtransparency="true"></iframe>
+        </div>
+    )
 }
