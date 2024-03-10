fe94f1f9fd49bf3002a7593692779e1bc9135a87
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-03-09 22:24:18 -0800

    set up form, emails not sending

--- src/pages/VolunteerApplication.jsx
+++ src/pages/VolunteerApplication.jsx
@@ -1,9 +1,48 @@
+import { useState } from 'react';
 import { css } from '@emotion/css';
 import { Link } from 'react-router-dom';
+import volunteerOpps from '../data/volunteerOpps.json';
+
+import emailjs from '@emailjs/browser';
+const serviceID = process.env.REACT_APP_SERVICE_ID;
+const templateID = process.env.REACT_APP_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_KEY;
 
 export default function VolunteerApplication() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [preferredContact, setPreferredContact] = useState('');
+    const [age, setAge] = useState('');
+    const [time, setTime] = useState('');
+    const [project, setProject] = useState('');
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        let templateParams = {
+            from_name: name,
+            message: {
+                name: name,
+                email: email,
+                phone: phone,
+                preferred_contact_method: preferredContact,
+                age: age,
+                time: time,
+                project_interested_in: project,
+            },
+            reply_to: email
+        }
+        console.log(templateParams);
+        
+        emailjs.send(serviceID, templateID, templateParams, publicKey)
+        .then(
+            (response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            },
+            (error) => {
+              console.log('FAILED...', error);
+            },
+          );
     }
 
     return (
@@ -14,40 +53,43 @@ export default function VolunteerApplication() {
             </header>
             <form onSubmit={handleSubmit} className={css` margin: 20px auto; display: flex; flex-direction: column;`}>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" placeholder="Name" required/>
+                <input type="text" id="name" placeholder="Name" value={name} onChange={event => setName(event.target.value)} required/>
 
                 <p>How should we contact you?</p>
                 <div className={css`display: flex; justify-content: space-around; align-items: center;`}>
                     <div>
-                        <input type="checkbox" id="phone" />
+                        <input type="radio" id="phone" name="contact" value="phone" onClick={event => setPreferredContact(event.target.value)}/>
                         <label htmlFor="phone">Phone</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="email" />
+                        <input type="radio" id="email" name="contact" value="email" onClick={event => setPreferredContact(event.target.value)}/>
                         <label htmlFor="email">Email</label>
                     </div>
                 </div>
 
                 <label htmlFor="email">Email Address</label>
-                <input type="text" id="email" placeholder="Email Address" required/>
+                <input type="text" id="email" placeholder="Email Address" value={email} onChange={event => setEmail(event.target.value)} required/>
 
                 <label htmlFor="phone">Phone Number</label>
-                <input type="tel" id="phone" placeholder="Phone Number" required/>
+                <input type="tel" id="phone" placeholder="Phone Number" value={phone} onChange={event => setPhone(event.target.value)} required/>
 
                 <div className={css`display: flex; justify-content: space-between; align-items: center;`}>
                     <div className={css`display: flex; flex-direction: column;`}>
                         <label htmlFor="age">Age</label>
-                        <input type="text" id="age" placeholder="Age" />
+                        <input type="text" id="age" placeholder="Age" value={age} onChange={event => setAge(event.target.value)} />
                     </div>
                     <div className={css`display: flex; flex-direction: column;`}>
                         <label htmlFor="availability">When are you open?</label>
-                        <input type="time" id="availability"  required/>
+                        <input type="time" id="availability" value={time} onChange={event => setTime(event.target.value)} required/>
                     </div>
                 </div>
 
                 <label htmlFor="project">I am available for these projects:</label>
-                <select id="project" required>
+                <select id="project" value={project} onChange={event => setProject(event.target.value)} required>
                     <option></option>
+                    {volunteerOpps.map((opp, index) =>
+                        <option key={index}>{opp.title}</option>
+                    )}
                 </select>
 
                 <input type="submit" />
