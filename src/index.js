3ed2772d2267c5d8d4c23d71594dfb1ae04d268a
Anita Chengalva <anita.chengalva@gmail.com>
2024-03-10 15:49:59 -0700

    updated react router for links

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Error from './pages/Error';
 import Home from './pages/Home';
+import About from './pages/About';
 import VolunteerPage from './pages/VolunteerPage';
 import DonationPage from './pages/DonationPage';
 import ResourcesPage from './pages/ResourcesPage';
@@ -23,6 +24,7 @@ root.render(
           <Route path="/volunteer" element={<VolunteerPage />} />
           <Route path="/donate" element={<DonationPage />} />
           <Route path="/pay" element={<PaymentPage />} />
+          <Route path="/about" element={<About />} />
           <Route path="/resources" element={<ResourcesPage />} />
           <Route path="/resources/websites" element={<WebsiteResources />} />
           <Route path="/news" element={<NewsPage />} />
