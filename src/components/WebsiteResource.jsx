26af8029060f5ca563a6fdf7fb30e980f1e29f7f
HUJOE003 <hpandise@calpoly.edu>
2024-03-24 23:26:14 -0700

    website resources (Hujoe)

--- src/components/WebsiteResource.jsx
+++ src/components/WebsiteResource.jsx
@@ -2,14 +2,15 @@ import { css } from '@emotion/css';
 
 export default function WebsiteResource({ name, description, image, link }) {
     return (
-        <div id="website-resource" className={css`display: flex; justify-content: space-evenly;`}>
+        <div id="website-resource" className={css`display: flex; justify-content: space-evenly`}>
             <div>
-                <img src={image} alt={name} />
+                <img className='websiteresourseimage' src={image} alt={name} />
             </div>
             <div>
-                <h3>{name}</h3>
-                <p>{description}</p>
-                <a href={link}>Learn More</a>
+                <h3 className='title'>{name}</h3>
+                <p className='description'>{description}</p>
+                <a className='learnmore' href={link}>Learn More</a>
+                <div class="arrow"></div>
             </div>
         </div>
     )
--- src/pages/WebsiteResources.jsx
+++ src/pages/WebsiteResources.jsx
@@ -38,12 +38,12 @@ export default function WebsiteResources() {
     return (
         <div>
             <header>
-                <h1>Website Resources</h1>
+                <h1 className='header'>Website Resources</h1>
             </header>
 
-            <main className={css`display: flex; flex-wrap: wrap; justify-content: space-evenly;`}>
+            <main className={css`display: flex; flex-wrap: wrap; justify-content: space-evenly;margin:200px`}>
                 {websiteResources.map((resource, index) =>
-                    <div key={index} className={css`width: 45%; margin: 20px; border: 1px solid lightgrey; padding: 30px;`}>
+                    <div key={index} className={css`height:273px;width: 493px; margin:10px;margin-bottom:10px;border: 2px solid lightgrey; padding: 40px; border-bottom:2px solid #97C159 `}>
                         <WebsiteResource name={resource.name} description={resource.description} image={resource.image} link={resource.link} />
                     </div>
                 )}
