fa71c34fe671015baacd4ff126ec2db600180e73
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-04-01 10:23:25 -0700

    merge main

--- src/components/Newsfeed.jsx
+++ src/components/Newsfeed.jsx
@@ -6,7 +6,7 @@ export default function Newsfeed() {
         <section id="news-feed">
         {blogPosts.map((post, index) =>
             <div key={index}>
-                <Link to={'/news/' + post.id} style={{ textDecoration: 'none' }}>
+                <Link to={'/phig-website/news/' + post.id} style={{ textDecoration: 'none' }}>
                     <div className="news-feed-article" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.40), rgba(0, 0, 0, 0.40)), url(${post.image})`}}>
                         <h3 className="news-feed-title">{post.title}</h3>
                         <p className="news-feed-author">{post.author}</p>
