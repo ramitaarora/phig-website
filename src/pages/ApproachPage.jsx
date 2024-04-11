b6e1e705b0a454af89ae168f1722b831ede29222
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-04-10 21:41:12 -0700

    initial layout

--- src/pages/ApproachPage.jsx
+++ src/pages/ApproachPage.jsx
@@ -1,3 +1,27 @@
 export default function ApproachPage() {
-    return;
+    return (
+        <div>
+        
+        <header></header>
+        <main>
+            <div>
+                <h1>Our Mission & Vision</h1>
+                <hr />
+                <h4>To educate students, staff, and community members in the need for water conservation with a demonstration garden using no pesticides, water recycling methods, drought tolerant and native plantings.</h4>
+                <h4>Safe and effective gardening tips & tricks for your home garden </h4>
+                <hr />
+                <h2>Our Goals for 2026</h2>
+                <p>We’re racing to hit these targets to help the world reverse climate change and biodiversity loss. Together, we find the paths to make change possible.</p>
+            </div>
+            <div>
+                <img src="./images/our-approach-poppies.jpeg" alt="our-approach-poppies" />
+                <h2>About us</h2>
+                <p>Volunteers, working together with various groups and organizations, transformed a former wasteland into a thriving wildlife habitat and water-conserving garden at Pleasant Hill Education Center. </p>
+                <p>The project began in Spring 2007 as a landscape design class initiative and culminated in a beautiful and sustainable space. Led by Teacher Monika Olsen, students from the Gardening Program, Boy Scouts, Girl Scouts, Contra Costa Master Gardeners, DVC's adaptive horticulture class, and other enthusiastic volunteers collaborated with the City of Pleasant Hill, Central Contra Costa Sanitary District, and other donors. </p>
+                <p>The official groundbreaking occurred on September 15, 2007, during Pleasant Hill's Community Service Day. The garden, situated near Pleasant Hill Middle School and behind the Pleasant Hill Library, serves as a demonstration garden showcasing sustainable practices such as pesticide-free methods, water conservation, recycling, and the use of drought-tolerant and native plants.</p>
+            </div>
+        </main>
+        
+        </div>
+    )
 }
