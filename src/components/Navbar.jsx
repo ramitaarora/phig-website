e0e1b5890338ff0fabfb2bc0a0dc21ece2734598
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-05-03 13:38:23 -0700

    changed nav carats to svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
         <div className="nav-dropdown">
           <Link to="/contact">
             About Us
-            <img src="./images/nav-carat-down.svg" alt="down" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="white" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+            </svg>
           </Link>
           <div className="nav-dropdown-content">
             <Link to="/contact">Contact</Link>
@@ -26,7 +28,9 @@ const Navbar = () => {
         <div className="nav-dropdown">
           <Link to="/news">
             News
-            <img src="./images/nav-carat-down.svg" alt="down" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="white" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+            </svg>
           </Link>
           <div className="nav-dropdown-content">
             <Link to="/events">Attend Events</Link>
@@ -37,7 +41,9 @@ const Navbar = () => {
         <div className="nav-dropdown">
           <Link to='/volunteer'>
             Get Involved
-            <img src="./images/nav-carat-down.svg" alt="down" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="white" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+            </svg>
           </Link>
           <div className="nav-dropdown-content">
             <Link to="/projects">Projects</Link>
