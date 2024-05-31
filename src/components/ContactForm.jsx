a5a23ae19f806d9ea27689b7113253b825b25a36
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-05-31 14:49:11 -0700

    about page form styling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import emailjs from '@emailjs/browser';
+import { css } from '@emotion/css';
 
 export default function ContactForm() {
     const navigate = useNavigate()
@@ -20,7 +21,7 @@ export default function ContactForm() {
     const handleSubmit = (event) => {
         event.preventDefault();
         setErrorMessage([]);
-        
+
         const testEmail = (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i).test(email);
         const testPhone = (/^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/i).test(phone);
 
@@ -40,61 +41,70 @@ export default function ContactForm() {
             }
 
             emailjs.send(process.env.REACT_APP_SERVICE_KEY, process.env.REACT_APP_TEMPLATE_KEY, templateParams, process.env.REACT_APP_PUBLIC_KEY)
-            .then(
-                (response) => {
-                // console.log('SUCCESS!', response.status, response.text);
-                navigate('/about/confirmation', { replace: true });
-                setFirstName('');
-                setLastName('');
-                setFullName('');
-                setEmail('');
-                setPhone('');
-                setCountry('');
-                setText('');
-                },
-                (error) => {
-                // console.log('FAILED...', error);
-                setErrorMessage(pre => [...pre, "Error: Email not sent. Please contact us at PHinstructionalgarden@gmail.com"]);
-                },
-            );
+                .then(
+                    (response) => {
+                        // console.log('SUCCESS!', response.status, response.text);
+                        navigate('/about/confirmation', { replace: true });
+                        setFirstName('');
+                        setLastName('');
+                        setFullName('');
+                        setEmail('');
+                        setPhone('');
+                        setCountry('');
+                        setText('');
+                    },
+                    (error) => {
+                        // console.log('FAILED...', error);
+                        setErrorMessage(pre => [...pre, "Error: Email not sent. Please contact us at PHinstructionalgarden@gmail.com"]);
+                    },
+                );
         }
     }
     return (
         <div id="contact-form" className="about-page-contact">
-            <h2>How Can We Help?</h2>
+            <h2 className={css`text-align: center;`}>How Can We Help?</h2>
             <form action="#" method="post" onSubmit={handleSubmit}>
-                <div>
-                    <input type="text" id="first-name" name="first-name" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="First Name" required />
+                <div className={css`width: 100%; display: flex; justify-content: space-between; align-items: center;`}>
+                    <div className={css`display: flex; flex-direction: column; align-items: flex-start; width: 100%; margin-right: 10px;`}>
+                        <label htmlFor="first-name">First Name<span>*</span></label>
+                        <input className="about-page-form-input contact-form-input" type="text" id="first-name" name="first-name" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="First Name" required />
+                    </div>
+                    <div className={css`display: flex; flex-direction: column; align-items: flex-start; width: 100%; margin-left: 10px;`}>
+                        <label htmlFor="last-name">Last Name<span>*</span></label>
+                        <input className="about-page-form-input contact-form-input" type="text" id="last-name" name="last-name" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="First Name" required />
+                    </div>
                 </div>
                 <div>
-                    <input type="text" id="last-name" name="last-name" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="First Name" required />
+                    <label htmlFor="email">Email<span>*</span></label>
+                    <input className="about-page-form-input contact-form-input" type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" required />
                 </div>
-                <div>
-                    <label htmlFor="email">Email*  </label>
-                    <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required />
-                </div>
-                <div>
-                    <label htmlFor="phone">Phone Number*  </label>
-                    <select id="country-code" name="country-code" required>
+
+                <div className={css`display: flex; flex-direction: column; align-items: flex-start;`}>
+                <label htmlFor="phone">Phone Number<span>*</span></label>
+                <div className={css`display: flex; width: 100%;`}>
+                    <select className="about-page-form-input" id="country-code" name="country-code" required>
                         <option value="+1">+1 (USA)</option>
                         <option value="+44">+44 (UK)</option>
                         <option value="+91">+91 (India)</option>
                     </select>
-                    <input type="text" id="phone" name="phone" value={phone} onChange={e => setPhone(e.target.value)} required />
+                    <input className="about-page-form-input contact-form-input" type="text" id="phone" name="phone" value={phone} onChange={e => setPhone(e.target.value)} placeholder="Phone Number" required />
                 </div>
+                </div>
+
+                
                 <div>
-                    <label htmlFor="country">Country of Residence  </label>
-                    <select id="country" name="country" value={country} onChange={e => setCountry(e.target.value)} required>
+                    <label htmlFor="country">Country of Residence</label>
+                    <select className="about-page-form-input contact-form-input" id="country" name="country" value={country} onChange={e => setCountry(e.target.value)} required>
                         <option value="USA">USA</option>
                         <option value="UK">UK</option>
                         <option value="India">India</option>
                     </select>
                 </div>
-                <div>
-                    <label htmlFor="message">Message  </label>
-                    <textarea id="message" name="message" rows="4" cols="50" value={text} onChange={e => setText(e.target.value)} required></textarea>
+                <div className={css`display: flex; flex-direction: column; align-items: flex-start;`}>
+                    <label htmlFor="message">Message<span>*</span></label>
+                    <textarea contact-form-input className="about-page-form-input contact-form-input" id="message" name="message" rows="4" cols="50" value={text} onChange={e => setText(e.target.value)} placeholder="Message" required></textarea>
                 </div>
-                <input type="submit" />
+                <input type="submit" className="green-button" />
             </form>
             <div id="error-messages">
                 {errorMessage.length ? (
