42e49629829e17b0bf4a48c8636a0793def609da
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-18 14:09:01 -0700

    leaderboard mobile styling

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -3,18 +3,18 @@ import contributors from '../data/contributers.json';
 
 export default function Leaderboard() {
     return (
-        <section id="leaderboard" className={css`margin: 90px auto; width: 50%; text-align: center; font-family: "Inria Sans", sans-serif; @media (max-width: 768px) { display: none; }`}>
+        <section id="leaderboard" className={css`margin: 90px auto; width: 80%; text-align: center; font-family: "Inria Sans", sans-serif; @media (max-width: 768px) { width: 90%; }`}>
 
             <div id="leaderboard-header">
                 <h2 style={{fontWeight:'500'}}>Leaderboard</h2>
                 <p style={{fontSize: 'small', color:'grey', marginTop:'15px',marginBottom:'50px'}}>A special thank you to our recent contributors</p>
             </div>
 
-            <div id="leaderboard-filter" className={css`display: flex; justify-content: space-between; align-items: center; margin-bottom: 50px`}>
+            <div id="leaderboard-filter" className={css`display: flex; justify-content: space-between; align-items: center; margin-bottom: 50px; @media (max-width: 768px) { flex-direction: column; }`}>
                 <div >
                     <input style={{color: 'grey',padding: '20px',border: 'none', boxShadow: '3px 3px 3px 3px lightgrey',height:'50px',width:'250px',borderRadius:'30px'}} type="text" name="search" placeholder="Search"/>
                 </div>
-                <div>
+                <div className={css`@media (max-width: 800px) { margin: 20px; }`}>
                     <button className='pill-button'>Contributions</button>
                     <button className='pill-button-outline'>Special Volunteers</button>
                 </div>
